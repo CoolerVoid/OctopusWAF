cb2c47702170142dd8f50859b358139916251ae9
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2020-11-28 12:18:52 -0300

    If the intent if to check for write errors, we should check for fprintf() return value as well.

--- src/file_ops.c
+++ src/file_ops.c
@@ -3,30 +3,30 @@
 
 
 // write line in file
-void WriteFile(char *file,char *str)
+void WriteFile ( char *file, char *str )
 {
 	FILE *arq;
- 
-	arq=fopen(file,"a"); 
+  int status;
 
-	if ( arq == NULL ) 
-	{
-//		fclose(arq);
-		DEBUG("error in WriteFile() %s",file);
-	        exit(0);
-		
-	}
+	arq = fopen ( file, "a" );
 
-	fprintf(arq,"%s\n",str); 
+	if ( arq == NULL )
+		{
+      // fclose(arq);
+			DEBUG ( "error in WriteFile() %s", file );
+			exit ( 0 ); // FIXME: Shoudn't be different than 0?
+		}
 
-	if( fclose(arq) == EOF )
-	{
-		DEBUG("error in Write() file %s",file);
-		exit(0);
-	}
-	arq=NULL;
- 
+  // FIX: If you want really to check for write errors, remember printf() family of functions
+  //      return a negative value in case of errors!
+	status = fprintf ( arq, "%s\n", str );
 
+  // FIX: || is a sequence point, so this if() is correct.
+	if ( fclose ( arq ) == EOF || status < 0 )
+		{
+			DEBUG ( "error in Write() file %s", file );
+			exit ( 0 ); // FIXME: Shouln't be different than 0?
+		}
 }
 
 
