2e186d29f8e65499ab855ac009dcbf92440ab42f
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2020-11-29 14:11:30 -0300

    Got rid of is_null_argv() and changed is_request() to correct the possible problem to use an invalid http method.

--- src/validate.c
+++ src/validate.c
@@ -1,50 +1,32 @@
+#include <string.h>
+
 #include "utils.h"
 #include "validate.h"
 
-void isnull_argv()
+bool is_request ( char *ptr )
 {
+	const char **p;
 
-  if ( strnlen ( param.hostarg, 256 ) == 0 )
-    die ( "Need host argument\n" );
+  // FIXME: OPTIONS, CONNECT, etc?
+	static const char *methods[] =
+	{
+		"GET ", "PUT ", "POST ", "DELETE ", NULL
+	};
 
-  if ( strnlen ( param.redirectarg, 256 ) == 0 )
-    die ( "Need redirect argument" );
+  // FIX: Don't need to check for spaces at begining.
+  // FIXME: Why ptr must have specfically more than 9 chars?
+	if ( ptr && strnlen ( ptr, 12 ) >= 10 )
+		{
+			p = methods;
 
-  // FIX: I think this is clearer
-  if ( param.option_algorithm < 1 || param.option_algorithm > 4 )
-    die ( "Need log argument" );
-}
+			while ( *p )
+				{
+					if ( memcmp ( ptr, *p, strlen ( *p ) ) == 0 )
+						return true;
 
+					p++;
+				}
+		}
 
-
-bool is_request(char *ptr)
-{
-	if(ptr == NULL)
-		return false;
-
-	if(ptr[0]==' ')
-		return false;
-
-	if(ptr[1]==' ')
-		return false;
-
-	if(strnlen(ptr,12) < 10)
-		return false;
-
-// is GET ?
- 	if(ptr[0]=='G' && ptr[1]=='E' && ptr[2]=='T')
-		return true;
-
-// is POST ?
- 	if(ptr[0]=='P' && ptr[1]=='O' && ptr[2]=='S' && ptr[3]=='T')
-		return true;
-
-// is PUT ?
- 	if(ptr[0]=='P' && ptr[1]=='U' && ptr[2]=='T')
-		return true;
-// is DELETE ?
- 	if(ptr[0]=='D' && ptr[1]=='E' && ptr[2]=='L' && ptr[3]=='E' && ptr[4]=='T' && ptr[5]=='E')
-		return true;
-
- return false;
+	return false;
 }
