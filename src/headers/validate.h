ea169ed9b1f080a7bb512039e54814143d6c8ec7
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2020-11-29 14:19:44 -0300

    Got rid of unecessary includes.

--- src/headers/validate.h
+++ src/headers/validate.h
@@ -1,12 +1,8 @@
 #ifndef VALIDATE_H__
 #define VALIDATE_H__
 
-#include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
-#include "opt_extract.h"
 
-void isnull_argv();
 bool is_request ( char *ptr );
 
 #endif
